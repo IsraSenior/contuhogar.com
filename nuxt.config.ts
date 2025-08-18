69014784877a5db329f2019610aadaccd4b00502
Israel Senior <israsenior.dev@gmail.com>
2025-08-18 12:49:49 -0400

    gtag

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,5 +14,14 @@ export default defineNuxtConfig({
   //   pageTransition: { name: "page", mode: "out-in" },
   // },
 
-  modules: ["vue3-carousel-nuxt", "@pinia/nuxt"],
+  modules: ["vue3-carousel-nuxt", "@pinia/nuxt", "nuxt-gtag"],
+
+  gtag: {
+    enabled: process.env.NODE_ENV === 'production',
+    // Your primary Google tag ID
+    id: 'G-1182NP1Z0D',
+    // Additional configuration for this tag ID
+    config: {
+    },
+  }
 });
